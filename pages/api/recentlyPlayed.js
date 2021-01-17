63ab66cbcb612e98b9d85a4161ee6319037ebc8b
Kristen Cabrera <cabrera.kristen@gmail.com>
2021-01-17 17:29:42 -0600

    first commit~~~

---
+++ pages/api/recentlyPlayed.js
@@ -0,0 +1,31 @@
+import { getRecentlyPlayed } from '../../lib/spotify';
+
+export default async (_, res) => {
+  const response = await getRecentlyPlayed();
+  const { items } = await response.json();
+
+  const allAlbums = items.map((item) => ({
+    album: item.album.name,
+    images: item.album.images,
+    artist: item.album.artists.map((_artist) => _artist.name).join(' + ')
+  }));
+
+  const uniqueAlbums = [];
+  const map = new Map();
+  for (const item of allAlbums) {
+    if (uniqueAlbums.length < 6) {
+      if (!map.has(item.album)) {
+        map.set(item.album, true);    // set any value to Map
+        uniqueAlbums.push({
+          album: item.album,
+          images: item.images,
+          artist: item.artist
+        });
+      }
+    }
+  }
+
+  // const uniqueAlbums = uniqueAlbums.slice(0, 6);
+
+  return res.status(200).json({ uniqueAlbums });
+};
