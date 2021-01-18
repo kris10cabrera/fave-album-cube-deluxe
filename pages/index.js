95da518b136581194667a3e500d617f76484da63
Kristen Cabrera <cabrera.kristen@gmail.com>
2021-01-17 20:46:50 -0600

    crzy styles

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,67 @@
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import fetcher from '../lib/fetcher';
 
 
 export default function Home() {
+  const [position, setPosition] = useState({ x: 50, y: 50 });
+  const [page, setPagePosition] = useState({ x: 200, y: 600 });
+
+  const moveAlbums = (e) => {
+    const x = e.pageX;
+    const y = e.pageY;
+    setPagePosition({ x: e.pageX, y: e.pageY });
+    if (window.innerWidth < 500) {
+      setPosition({ x: ((x - window.innerWidth / 2) * 2), y: ((y - window.innerHeight / 2) * 2) });
+    } else {
+      setPosition({ x: ((x - window.innerWidth / 2) * .5), y: ((y - window.innerHeight / 2) * .5) });
+    };
+  };
+
+  useEffect(() => {
+    addEventListeners();
+    return () => removeEventListeners();
+  }, []);
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", moveAlbums);
+    document.addEventListener("touchmove", moveAlbums);
+  };
+
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", moveAlbums);
+    document.removeEventListener("touchmove", moveAlbums);
+  };
   const { data } = useSWR('/api/recentlyPlayed', fetcher);
   if (!data) {
     return null;
   }
-  const albums = data.uniqueAlbums;
 
-  const names = ['cover', 'spine', 'back', 'top', 'side', 'bottom'];
+  const albums = data.uniqueAlbums;
+  const names = ['top', 'side', 'cover', 'spine', 'back', 'bottom'];
+
+  let cubeStyle = {
+    transform: `rotateX(${position.y}deg) rotateY(${position.x}deg) `,
+    left: `${page.x}px`,
+    top: `${page.y}px`
+  }
   return (
     <div >
       <Head>
-        <title>fave album cube, deluxe</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>fave album cube by kris10cabrera</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       <main>
-        album
+        <header>
+          <h1>here is a cube of my favorite albums</h1>
+          <span>*of the last 4 weeks. made by <a href="https://www.kristencabrera.com/">kris10cabrera</a> using the spotify api</span>
+          
+        </header>
         <section className="parent">
-          <section className="cube">
+          <section className="cube" style={cubeStyle}>
             {albums.map((album, index) => {
               let albumStyle = {
                 backgroundImage: 'url(' + album.images[0].url + ')',
@@ -30,9 +70,7 @@ export default function Home() {
             })}
           </section>
         </section>
-
       </main>
-
     </div>
   )
 }
